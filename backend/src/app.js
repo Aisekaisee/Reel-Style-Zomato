2b8b6fecdeb871347ee0b94439ce9eb3c32c4c20
Aisekaisee <kohlivipul48@gmail.com>
2026-03-18 21:59:13 +0530

    implemented cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,13 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173', // Update this to match your frontend URL
+  credentials: true, // Allow cookies to be sent with requests
+}));
 app.use(express.json());
 app.use(cookieParser());
 
